632044506a8e3f24554234a37009bcfd2579841c
bradleyhop <bradleyhop@gmail.com>
2022-11-30 18:31:36 -0500

    portfolio list items with img and links

--- src/components/PortCard.vue
+++ src/components/PortCard.vue
@@ -1,7 +1,6 @@
 <script>
 // load icons
-import { mdiOpenInNew } from "@mdi/js";
-import { mdiGithub } from "@mdi/js";
+import { mdiOpenInNew, mdiGithub } from "@mdi/js";
 
 export default {
   name: "PortCard",
--- src/components/PortfolioCollapse.vue
+++ src/components/PortfolioCollapse.vue
@@ -1,6 +1,8 @@
 <script>
 import { Collapse } from "vue-collapsed";
 import { reactive } from "vue";
+// load icons
+import { mdiOpenInNew, mdiGithub } from "@mdi/js";
 
 export default {
   name: "PortfolioCollapse",
@@ -8,6 +10,12 @@ export default {
   components: {
     Collapse,
   },
+
+  data: () => ({
+    // mdi icons
+    mdiOpenInNew,
+    mdiGithub,
+  }),
 
   props: {
     dataList: {
@@ -38,17 +46,74 @@ export default {
 </script>
 
 <template>
-  <div v-for="(topic, index) in portList" :key="topic.title">
-    <h4 @click="() => handleAccordion(index)" class="topic-collapse">
+  <div
+    v-for="(topic, index) in portList"
+    :key="topic.title"
+    class="collapse-container"
+  >
+    <h5 @click="() => handleAccordion(index)" class="topic-collapse">
       {{ topic.title }}
-    </h4>
+    </h5>
     <Collapse :when="portList[index].isExpanded" class="collapse">
       <p class="topic-copy">
         {{ topic.copy }}
       </p>
-      <ul v-for="project in portList[index].projects" :key="project.title">
-        <li class="project-item">
-          {{ project.title }}
+      <ul class="topic-list">
+        <li
+          v-for="project in portList[index].projects"
+          :key="project.title"
+          class="project-item"
+        >
+          <img
+            :src="`./screenshots/${project.img.src}`"
+            :alt="project.img.alt"
+            class="project-img"
+          />
+          <div class="links-container">
+            <p>{{ project.title }}</p>
+            <div class="button-container">
+              <a
+                :href="project.link"
+                class="project-link"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="live demo"
+              >
+                <button class="link-button">
+                  <span class="button-text">Live Site</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    alt="image indicating an outside link"
+                    viewBox="0 0 24 24"
+                    class="svg-icon"
+                  >
+                    <path :d="mdiOpenInNew"></path>
+                  </svg>
+                </button>
+              </a>
+              <a
+                :href="project.github"
+                class="project-link"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="source code"
+              >
+                <button class="link-button">
+                  <span class="button-text">GitHub Repo</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    aria-hidden="true"
+                    viewBox="0 0 24 24"
+                    class="svg-icon"
+                  >
+                    <path :d="mdiGithub"></path>
+                  </svg>
+                </button>
+              </a>
+            </div>
+          </div>
         </li>
       </ul>
     </Collapse>
@@ -60,12 +125,87 @@ export default {
 .collapse {
   transition: height var(--vc-auto-duration) cubic-bezier(0.3, 0, 0.6, 1);
 }
+
+.collapse-container {
+  padding: 0.5rem;
+  border-bottom: 1px solid lightgray;
+}
 // component styling
 .topic-collapse {
   cursor: pointer;
+  font-size: 1rem;
+  font-weight: normal;
+  letter-spacing: 0.5px;
+  padding-bottom: 0.7rem;
 
   &:hover {
     text-decoration: underline;
+  }
+}
+
+.project-img {
+box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  height: 3.5rem;
+  margin-right: 1rem;
+  object-fit: cover;
+}
+
+.topic-container {
+  margin: 0.75rem 0;
+}
+
+.topic-copy {
+  margin-bottom: 1rem;
+}
+
+.topic-list {
+  list-style: none;
+}
+
+.project-item {
+  padding-bottom: 1rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.button-container {
+  display: flex;
+  align-items: center;
+}
+
+.project-link {
+  padding: 0.5rem 1rem 0.5rem 0;
+
+  .links-container {
+    display: block;
+  }
+
+  .link-button {
+    display: inline-flex;
+    border: none;
+    background-color: $lightest-blue;
+    border-radius: 100px;
+    cursor: pointer;
+    font-size: 0.7rem;
+    padding: 0.4rem 0.75rem;
+
+    &:hover {
+      transform: scale(1.1);
+      transition-duration: 200ms;
+    }
+
+    .button-text {
+      align-self: center;
+      color: $not-black;
+      padding-right: 4px;
+    }
+
+    .svg-icon {
+      fill: $not-black;
+      height: 1.5em;
+      width: 1.5em;
+    }
   }
 }
 </style>
