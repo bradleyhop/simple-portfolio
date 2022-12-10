acd17a4e16b951f367033920758ff537729627c5
bradleyhop <bradleyhop@gmail.com>
2022-12-10 14:01:23 -0500

    form logic

--- src/views/ContactMe.vue
+++ src/views/ContactMe.vue
@@ -17,6 +17,46 @@ export default {
       default: "",
     },
   },
+
+  data: () => {
+    const defaultForm = Object.freeze({
+      email: "",
+      message: "",
+    });
+
+    return {
+      form: Object.assign({}, defaultForm),
+      textBodyDefault: "message",
+      defaultForm, // form object
+    };
+  },
+
+  methods: {
+    submit() {
+      // we're using Netlify to handle form submission; here's their article on doing so with Vue:
+      // https://www.netlify.com/blog/2018/09/07/how-to-integrate-netlify-forms-in-a-vue-app/
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: this.encode({
+          "form-name": "contact-me",
+          ...this.form,
+        }),
+      })
+      // TODO: replace these dummy vue-data messages with a div?
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(response.statusText);
+          } else {
+            this.textSnackbar = "Form submitted! Thank you!! ";
+          }
+        })
+        .catch((error) => {
+          this.textSnackbar = "Error! Form not submitted: ";
+          this.textSnackbar += error;
+        });
+    },
+  },
 };
 </script>
 
@@ -24,11 +64,23 @@ export default {
   <PageHeader pageTitle="Contact" />
 
   <main class="device-widths">
-    <form class="contact-form" name="contact-me" method="post" accept-charset="UTF-8">
-      <label class="contact-label" for="from-email">Your Email</label>
-      <input class="textbox" id="from-email" type="email" name="fromEmail" />
+    <form
+      class="contact-form"
+      name="contact-me"
+      method="post"
+      accept-charset="UTF-8"
+    >
+      <label class="contact-label" for="email">Your Email</label>
+      <input
+        class="textbox"
+        id="email"
+        type="email"
+        name="fromEmail"
+        required
+      />
       <label class="contact-label message-label" for="message">Message</label>
-      <textarea class="textbox" rows="6" id="message" name="message" />
+      <textarea class="textbox" rows="6" id="message" name="message" required />
+      <input type="submit" class="submit-form-input" value="Send Message" />
     </form>
   </main>
 
@@ -57,5 +109,18 @@ export default {
   font-size: 1.25rem;
   max-width: 40%;
   padding: 0.5rem;
+}
+
+.submit-form-input {
+  background-color: $lightest-blue;
+  border: none;
+  border-radius: 5px;
+  color: $not-black;
+  cursor: pointer;
+  font-family: "Raleway", sans-serif;
+  letter-spacing: 0.5px;
+  margin-top: 1.5rem;
+  padding: 1rem;
+  max-width: 10rem;
 }
 </style>
