ae7a62e426838a9e130f9ad3eefd3be42042bb7c
bradleyhop <bradleyhop@gmail.com>
2022-12-20 17:40:58 -0500

    forgot to remove imports

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import webfontDownload from "vite-plugin-webfont-dl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
