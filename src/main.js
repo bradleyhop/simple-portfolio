875cac0b07ee551183abb181e8a36e7a5915d0d6
bradleyhop <bradleyhop@gmail.com>
2023-01-31 14:10:43 -0500

    router debug

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import { createApp } from "vue";
-import App from "./App.vue";
+import App from "@/App.vue";
 import router from "@/router";
 import VueGtag from "vue-gtag";
 
-const app = createApp(App);
-
-app.use(router).use(VueGtag, {
-  config: { id: "G-PCJSPMG67T" },
-});
-
-app.mount("#app");
+createApp(App)
+  .use(router)
+  .use(VueGtag, {
+    config: { id: "G-PCJSPMG67T" },
+  })
+  .mount("#app");
