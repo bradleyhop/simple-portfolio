e50a6184c8359650fda425b384ca946afae82298
bradleyhop <bradleyhop@gmail.com>
2023-01-31 15:41:10 -0500

    update readme; code cleanup

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
-// import SitePortfolio from "@/views/SitePortfolio.vue";
-// import ContactMe from "@/views/ContactMe.vue";
 
 const router = createRouter({
-  // history: createWebHistory(import.meta.env.BASE_URL),
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
 
   routes: [
     {
@@ -16,13 +13,11 @@ const router = createRouter({
     {
       path: "/portfolio",
       name: "SitePortfolio",
-      // component: SitePortfolio,
       component: () => import("@/views/SitePortfolio.vue"),
     },
     {
       path: "/contact",
       name: "ContactMe",
-      // component: ContactMe,
       component: () => import("@/views/ContactMe.vue"),
     },
   ],
