9cca8b83dfcfe15eec3f3aff229c4bbd2f08898b
Bradley Smith <bradleyhop@gmail.com>
2023-08-25 14:42:07 -0400

    edit copy

--- src/assets/data/recentProjects.js
+++ src/assets/data/recentProjects.js
@@ -3,8 +3,8 @@ const recents = [
     id: 1,
     title: "Expense Chart Component",
     body: `A Frontend Mentor Project, this is a expense report with a bar
-      chart. The data is loaded via local json and the chart is drawn using
-      only CSS. Built from a professionally designed Figma file using Vue,
+      chart. The data is loaded via local JSON and the chart is drawn using
+      only CSS. Built from a professionally designed Figma file using Vue.js,
       Vite, and Sass.`,
     link: "https://github.com/bradleyhop/frontend-mentor-expenses-chart-component",
     linkTitle: "Fronent Mentor Expense Chart Component",
@@ -14,7 +14,7 @@ const recents = [
     id: 2,
     title: "Professional Portfolio",
     body: `Update and redesign client's professional portfolio page. Built
-      in WordPress with Elementor, custom CSS.`,
+      in WordPress with Elementor and custom CSS.`,
     link: "https://bainstructionaldesign.com",
     linkTitle: "Bradley Adamczyk Portfolio",
     linkText: "Bradley Adamczyk Portfolio",
@@ -22,8 +22,8 @@ const recents = [
   {
     id: 3,
     title: "Plant Facts Site",
-    body: `A Frontend Mentor project, this is an SPA fact site for planets
-      in our solar system. Built from a professionally designed Figma file
+    body: `A Frontend Mentor project; a Single Page Application fact site for planets
+      in our solar system. Built from a supllied professionally designed Figma file
       using Vue.js 3, Vue Router, Vite, and Sass.`,
     link: "https://bradleyhop.github.io/frontend-mentor-planet-facts-site/",
     linkTitle: "Frontend Mentor Planet Fact Site",
